3edecc7e607c394418804e01b9222ad6ef75bcd1
lgstuehrmann <lgstuehrmann18@gmail.com>
2017-11-21 15:16:08 -0500

    worked on color processing functions

--- pythonScripts/Stuehrmannfunctions.py
+++ pythonScripts/Stuehrmannfunctions.py
@@ -6,3 +6,22 @@ def UpdateTransforms(UAV_pos, UGV_pos):
 #Function to filter UAV image for two points of color, find the midpoint of the
 #resultant line, and find the line orientation.
 def ColorProcessing(cam_img, start_color, end_color):
+	thresholded = thresholdImg(cam_img, start_color, end_color)
+	UGV_camframepos = getMidpoint(thresholded)
+	UGV_camframerot = getAngle(thresholded)
+	UGV_campose = [UGV_camframepos, UGV_camframerot]
+	return UGV_campose
+
+def thresholdImg(cam_img, start_color, end_color):
+	
+	return thresholded
+
+def getMidpoint(image):
+
+	return xyzcoords
+
+def getAngle(image):
+
+	return anglerot
+
+
