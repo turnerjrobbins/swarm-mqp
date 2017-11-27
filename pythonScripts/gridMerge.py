1d1fd05697408c280d796128ac9729445d8e831f
Mathew Schwartzman <mathew.s.7196@outlook.com>
2017-11-27 16:41:30 -0500

    functions to translate up

--- pythonScripts/gridMerge.py
+++ pythonScripts/gridMerge.py
@@ -9,7 +9,7 @@ rows = 5
 cols = 5
 
 #########-------TEST ARRAYS------#######
-a = np.array([[0,0,0,0,0],
+a = np.array([[1,0,0,0,0],
 			  [0,0,0,0,0],
 			  [0,0,0,1,0],
 			  [0,0,1,2,1],
@@ -27,16 +27,30 @@ c = np.array([[0,0,0,0,0],
 			  [0,0,1,0,0],
 			  [0,0,0,0,0]])
 
-
-def similarity(matrix1, matrix2):
+def scalarDelta(matrix1, matrix2):	#simple similarity function that returns the absolute scalar differences between cell values
 	deltaMatrix = matrix1 - matrix2
 	deltaScalar = 0
 	for el in deltaMatrix.flatten():
 		deltaScalar = deltaScalar + abs(el)
 	return deltaScalar
 
+def translateUpOne(array):
+	d = np.zeros(rows*cols).reshape(rows, cols)	#create an array of zeros
+	array_flat = array.flatten()	#flatten the input array
+	for el in range(cols, (rows*cols)):	#start on second row
+		np.put(d, el-cols, array_flat[el])	#put all the elements up in the new array d
+	return d
+
+def translateUp(distance, array):
+	d = np.zeros(rows*cols).reshape(rows, cols)	#create an array of zeros
+	array_flat = array.flatten()	#flatten the input array
+	for el in range(cols*distance, (rows*cols)):	#start on second row
+		np.put(d, el-cols*distance, array_flat[el])	#put all the elements up in the new array d
+	return d
+
 print a
 print b
 print c
-print similarity(c,b)
+print scalarDelta(a,b)
+print translateUp(1, c)
 
