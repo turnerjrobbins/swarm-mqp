b4bfdc685a2b103a6082ccfbd59ca0f403723577
lgstuehrmann <lgstuehrmann18@gmail.com>
2017-11-27 18:08:55 -0500

    Continued working on thresholding functions.

--- pythonScripts/Stuehrmannfunctions.py
+++ pythonScripts/Stuehrmannfunctions.py
@@ -1,6 +1,6 @@
 #Function to update transform frame after robot global position changes
 #Takes in coordinates of UAV in global frame and coordinates of UGV in UAV camera frame
-def UpdateTransforms(UAV_pos, UGV_pos):
+def TransformtoGlobal(UAV_pos, UGV_pos):
 
 
 #Function to filter UAV image for two points of color, find the midpoint of the
@@ -15,11 +15,22 @@ def ColorProcessing(cam_img, start_color, end_color):
 def thresholdImg(cam_img, start_color, end_color):
 	
 	return thresholded
-
+#finds the midpoint of a line formed by 2 points in an image - used to determine where the robot is in the UAV's frame of reference
 def getMidpoint(image):
+	centroids = getCentroids(image)
+	x1 = centroids[1].x
+	x2 = centroids[2].x
+	y1 = centroids[1].y
+	y2 = centroids[2].y
+	xmid = abs(x1+x2)/2
+	ymid = abs(y1+y2)/2
+	xycoords = [xmid ymid]
+	return xycoords
 
-	return xyzcoords
+def getCentroids(image):
 
+	return centroids
+#angle of line in image; shows orientation of the robot with respect to UAV reference frame
 def getAngle(image):
 
 	return anglerot
