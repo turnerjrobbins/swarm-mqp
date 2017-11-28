9447ea97297b9cec39bd45cc1b34a28bd6e14afd
lgstuehrmann <lgstuehrmann18@gmail.com>
2017-11-27 19:34:29 -0500

    finished getAngle and getMidpoint - Need to figure out how to do image processing to get centroids and threshold the input from the camera

--- pythonScripts/Stuehrmannfunctions.py
+++ pythonScripts/Stuehrmannfunctions.py
@@ -1,8 +1,11 @@
-#Function to update transform frame after robot global position changes
+import numpy
+import openCV
+
+#Function to transform UGV coords to global
 #Takes in coordinates of UAV in global frame and coordinates of UGV in UAV camera frame
 def TransformtoGlobal(UAV_pos, UGV_pos):
-
-
+	
+	return globalUGV
 #Function to filter UAV image for two points of color, find the midpoint of the
 #resultant line, and find the line orientation.
 def ColorProcessing(cam_img, start_color, end_color):
@@ -11,11 +14,13 @@ def ColorProcessing(cam_img, start_color, end_color):
 	UGV_camframerot = getAngle(thresholded)
 	UGV_campose = [UGV_camframepos, UGV_camframerot]
 	return UGV_campose
-
+#start_color and end_color are rgb values
 def thresholdImg(cam_img, start_color, end_color):
-	
-	return thresholded
-#finds the midpoint of a line formed by 2 points in an image - used to determine where the robot is in the UAV's frame of reference
+#TODO: figure out how opencv works and use it for this
+	return image
+
+#finds the midpoint of a line formed by 2 points in an image - used to determine where
+#the robot is in the UAV's frame of reference
 def getMidpoint(image):
 	centroids = getCentroids(image)
 	x1 = centroids[1].x
@@ -26,13 +31,21 @@ def getMidpoint(image):
 	ymid = abs(y1+y2)/2
 	xycoords = [xmid ymid]
 	return xycoords
-
+#get the centers of circles in an image and return them as xy coordinate pairs in a list
 def getCentroids(image):
-
+#TODO: Figure out how opencv works and use it for this
 	return centroids
 #angle of line in image; shows orientation of the robot with respect to UAV reference frame
 def getAngle(image):
-
+	#make a triangle
+	centroids = getCentroids(image)
+	x1 = centroids[1].x
+	x2 = centroids[2].x
+	y1 = centroids[1].y
+	y2 = centroids[2].y
+	xleg = abs(x1-x2)+x1
+	yleg = abs(y1-y2)+y1
+	anglerot = arctan(yleg/xleg)
 	return anglerot
 
 
