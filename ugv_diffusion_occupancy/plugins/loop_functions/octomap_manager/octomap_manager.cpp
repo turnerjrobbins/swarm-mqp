099b4eec3f67e36ce9305b8b3e37ff83eaafbd3a
turner <tjrobbins@wpi.edu>
2018-01-29 17:27:21 -0500

    octomap compiles. Now just to get it interacting with the khepera bots

--- ugv_diffusion_occupancy/plugins/loop_functions/octomap_manager/octomap_manager.cpp
+++ ugv_diffusion_occupancy/plugins/loop_functions/octomap_manager/octomap_manager.cpp
@@ -1,1 +1,13 @@
 #include "octomap_manager.h"
+
+void CLoopOctomapManager::Init (TConfigurationNode& t_tree) {
+	;
+}
+
+void CLoopOctomapManager::insertRay() {
+	std::cout << "\n----insert ray called!----" << std::endl;
+}
+
+//CLoopOctomapManager::
+
+REGISTER_LOOP_FUNCTIONS(CLoopOctomapManager, "octomap_manager")
--- ugv_diffusion_occupancy/plugins/loop_functions/octomap_manager/octomap_manager.h
+++ ugv_diffusion_occupancy/plugins/loop_functions/octomap_manager/octomap_manager.h
@@ -1,4 +1,32 @@
 #ifndef OCTOMAP_MANAGER_H
 #define OCTOMAP_MANAGER_H
-//code goes here
+#include <argos3/core/simulator/loop_functions.h>
+#include <octomap/octomap.h>
+#include <octomap/OcTree.h>
+
+using namespace argos;
+using namespace octomap;
+
+class CLoopOctomapManager : public CLoopFunctions {
+public:
+	//Constructor
+	CLoopOctomapManager() : CLoopFunctions(), m_OcTree(OcTree(0.1)) {} //Argument is resolution?
+
+	//Destructor
+	~CLoopOctomapManager() {}
+
+	virtual void Init(TConfigurationNode& t_tree);
+
+	virtual void Reset() {}
+
+	virtual void Destroy() {}
+
+	virtual void PreStep() {}
+
+	virtual void PostStep() {}
+
+	void insertRay();
+private:
+	octomap::OcTree m_OcTree;
+};
 #endif
